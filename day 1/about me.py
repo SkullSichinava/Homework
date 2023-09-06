d8b43c2ad9d0b8d5337496ffb11c13a12c245ec4
SkullSichinava <sichinava614@gmail.com>
2023-09-07 00:46:51 +0400

    first commit

---
+++ day 1/about me.py
@@ -0,0 +1,22 @@
+# name = "luka"
+# surname = "sichinava"
+# print(name)
+name = "luka"
+surname = "sichinava"
+age = 15
+height = 175.0
+knows_programming = True
+is_ugly = False
+isUgly = False
+# print(name + " " + surname)
+# print(name + age)
+# print(type(age))
+# print(type(name))
+# print(type(surname))
+# print(type(height))
+# print(type(knows_programming))
+print(name + str(surname))
+print(age)
+print(height)
+print(knows_programming)
+print(is_ugly)
