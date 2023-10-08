5ab31cd495d8a9fb70facbb79e0f7e4edfbd6fc8
SkullSichinava <sichinava614@gmail.com>
2023-10-08 14:12:04 +0400

    commit

---
+++ day 5/lesson5.py
@@ -0,0 +1,10 @@
+user_score1 = float(input("enter score N1: "))
+
+avg_score = (user_score1)
+
+if avg_score >=10000:
+    print("შენი ქულაა: " + str(avg_score) + "  გოა-ში სწავლობდი?")
+elif avg_score >=1000 and avg_score <=10000:
+    print("შენი ქულაა: " + str(avg_score) + "  you mid")
+elif avg_score <=1000:
+    print("შენი ქულაა: " + str(avg_score) + "  შემოსულიყავი გოაში,მატრიცელო")
