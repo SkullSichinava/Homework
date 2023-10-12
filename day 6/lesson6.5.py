7b7b9172f2b46f2905ea13b58efcb694e374cf5a
SkullSichinava <sichinava614@gmail.com>
2023-10-12 16:22:44 +0400

    commit

---
+++ day 6/lesson6.5.py
@@ -0,0 +1,10 @@
+while True:
+    print("waking up")
+    print("having breakfast")
+    print("going to school")
+    print("going back to home")
+    print("having lunch")
+    print("doing homework")
+    print("studying at GOA")
+    print("going to sleep")
+    
---
+++ day 6/lesson6.py
@@ -0,0 +1,8 @@
+i = 1
+while i < 30:
+    if i % 2 == 0:
+        print(str(i) + " ლუწი")
+    else:
+        print(str(i) + " კენტი")
+    i += 1
+
