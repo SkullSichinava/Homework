8ad646e1eb6b3c219aa023cb32f227e72472b88d
SkullSichinava <sichinava614@gmail.com>
2023-11-01 22:36:09 +0400

    commit

--- day 5/homework/lesson5.py
+++ day 5/homework/lesson5.py
@@ -1,10 +1,27 @@
-user_score1 = float(input("enter score N1: "))
+my_age= 16
+mom_age= 44
+dad_age= 47
+brother_age= 5
 
-avg_score = (user_score1)
+my_age= my_age + 20
+mom_age= mom_age + 20
+dad_age= dad_age + 20
+brother_age= brother_age + 20
 
-if avg_score >=10000:
-    print("შენი ქულაა: " + str(avg_score) + "  გოა-ში სწავლობდი?")
-elif avg_score >=1000 and avg_score <=10000:
-    print("შენი ქულაა: " + str(avg_score) + "  you mid")
-elif avg_score <=1000:
-    print("შენი ქულაა: " + str(avg_score) + "  შემოსულიყავი გოაში,მატრიცელო")
+print(my_age)
+print(mom_age)
+print(dad_age)
+print(brother_age)
+
+
+my_age= int(input())
+mom_age= int(input())
+dad_age= int(input())
+brother_age= int(input())
+
+my_age= my_age + 20
+mom_age= mom_age + 20
+dad_age= dad_age + 20
+brother_age= brother_age + 20
+
+print(str(my_age)+" "+ str(mom_age)+" "+ str(dad_age)+" "+ str(brother_age))
--- day 9/homework/lesson9.py
+++ day 9/homework/lesson9.py
@@ -1,27 +1,2 @@
-old_list = [2, 4, 6, 2, 4, 7, 2, 9]
-new_list = []
-for num in old_list:
-    if num != 4:
-        new_list.append(num)
-print(new_list)
-
-
-family = ["Fatima", "Mamuka", "Luka", "Saba"]
-full_sentence = "My moms name is: {}, My dads name is: {}, My name is: {}, My brothers name is: {}".format(family[0],family[1],family[2],family[3])
-print(full_sentence)
-
-
-age = [44, 47, 15, 5]
-full_sentence = "My moms age is: {}, My dads age is: {}, My age is: {}, My brothers age is: {}".format(age[0],age[1],age[2],age[3])
-print(full_sentence)
-
-
-age = [54, 57, 25 ,15]
-full_sentence = "My moms age after 10 years is: {}, My dads age after 10 years is: {}, My age after 10 years is: {}, My brothers age after 10 years is: {}".format(age[0],age[1],age[2],age[3])
-print(full_sentence)
-
-
-joke = ["Let me tell you a joke",]
-full_sentence = "ერთ ებრაელს კითხეს, შემთხვევით ებრაელი ხომ არ ხართ? ებრაელმა უპასუხა, კი ებრაელი ვარ, ოხონთ შემთხვევით არა.".format(joke[0])
-print(full_sentence)
-
+for i in range (5, 100, 5):
+    print(i)  
