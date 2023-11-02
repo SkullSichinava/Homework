306b0b33963be9d952a56494fcb57a0958fe3c4f
SkullSichinava <sichinava614@gmail.com>
2023-11-02 17:34:52 +0400

    commit

---
+++ day 11/homework/lesson11.py
@@ -0,0 +1,15 @@
+def is_triangle(a, b, c):
+    if a + b > c and a + c > b and b + c > a:
+        print("ასეთი სამკუთხედი იარსებებს")
+    else:
+        print("ასეთი სამკუთხედი ვერ იარსებებს")
+
+is_triangle(5, 6, 7)
+ 
+
+
+def name_list(name1, name2, name3):
+    names_combined = ", ".join([name1, name2, name3])
+    print(names_combined)
+
+name_list("ლუკა","საბა","მამუკა")
