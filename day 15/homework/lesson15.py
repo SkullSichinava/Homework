27a7544b2446429ee21fe481293232088dc25203
SkullSichinava <sichinava614@gmail.com>
2023-11-16 17:05:14 +0400

    commit

---
+++ day 15/homework/lesson15.py
@@ -0,0 +1,101 @@
+# 1. What is the function used to display text in Python?
+print() #The print() function prints the specified message to the screen, or other standard output device. The message can be a string, or any other object, the object will be converted into a string before written to the screen.
+
+# 2. in Python, which data type is used to store a whole number?
+(int) # integer data type (int): represents whole numbers without any decimal points. It stores positive and negative whole numbers.
+
+# 3. Which of the following data types is used to store a text in Python?
+(str) # string data type (str): represents a sequence of characters enclosed in single quotes (‘ ‘) or double quotes (” “), such as “Hello, World!”, ‘Python’.
+
+# 4. What is the result of the expression 10 + 5 * 2 in Python?
+20 # the multiplication 5 * 2 which equals 10 is performed first, then resulting 10 + 10 which equals 20.
+
+# 5. Which of the following is correct way to assign the value 42 to a variable name answer in Python?
+answer = 42 # The assignment operator, denoted by the “=” symbol, is the operator that is used to assign values to variables in Python.
+
+# 6. What is the process of identifying and fixing errors in a program called?
+# Debugging is the process of finding and fixing errors or bugs in the source code of any software. When software does not work as expected, computer programmers study the code to determine why any errors occurred.
+
+# 7. Which is commonly used in python for naming variables and functions, where words separated?
+# Snake Case should be used for functions and variable names.
+
+# 8. What is the primary purpose of adding comments in Python?
+# The primary purpose of Python comments is to provide important information about what the code does and how it does it.
+
+# 9. How can you take user input in Python?
+# In Python, we use the input() function to ask the user for input. As a parameter, we input the text we want to display to the user. Once the user presses “enter,” the input value is returned. We typically store user input in a variable so that we can use the information in our program.
+
+# 10. Which of the following is not built-in data type in Python?
+# Array because it is a User-defined data type.
+
+# 11. What function can be used to check the data type of a variable in Python?
+# In Python, everything is an object. As a result, when you use the type() function to print the type of a variable's value to the console, it returns the class type of the object.
+
+# 12. How can you convert an integer to a string in Python?
+# In Python, you can convert an integer to a string using the str() function. For example, if you have a = 5 and you want to convert it to a string, you simply use a = str(5) . This would output '5' . You can also do this by casting the integer as a string like a = '5' .
+
+# 13. What is the for converting one data type into another in Python?
+# In Python, you can simply use the bin() function to convert from a decimal value to its corresponding binary value. And similarly, the int() function to convert a binary to its decimal value. The int() function takes as second argument the base of the number to be converted, which is 2 in the case of binary numbers.
+
+# 14. Which operator is used to check if two values are equal in Python?
+# The == operator compares the value or equality of two objects, whereas the Python is operator checks whether two variables point to the same object in memory.
+
+# 15. What is the result of the logical and operation between true and false in Python?
+# In Python, a logical expression that is true will compute to the value “True”. A false expression will compute to the value “False”.
+
+# 16. What will the expression (5>3) and (10<20) evaluate to in Python?
+# For both it will be true because 5 is greater than 3 and 20 is greater than 10.
+
+# 17. In Python, what is used to make decisions and execute different code blocks based on conditions?
+# We use the if-else statement is a control flow statement used in programming languages to make decisions based on conditions. It allows you to execute different blocks of code with certain conditions like True or False.
+
+# 18. Which type of loop is used to iterate over a sequence (e.g., a list or a string) in Python?
+# For Loop in Python is used to iterate over a sequence, which could be a list, tuple, array, or string.
+
+# 19. What type of loop is used when you what to repeat a block of code as long as a condition is true?
+# While Loop in Python is used to repeat a specific block of code an unknown number of times, until a condition is met. 
+
+# 20. What does the range() function in Python return?
+# The range() function returns a sequence of numbers, starting from 0 by default, and increments by 1 (by default), and stops before a specified number.
+
+# 21. Which keyword is used to start an if statement in Python?
+# The if keyword is used to create conditional statements (if statements), and allows you to execute a block of code only if a condition is True.
+
+# 22. What is the purpose of the "else" statement in Python's "if=else" construct?
+# The else statement is used to execute the false part of a given condition. 
+
+# 23. Which data structure is used to store an ordered collection of items in Python?
+# Tuple is a built-in data structure in Python that is an ordered collection of objects. Unlike lists, tuples come with limited functionality. 
+
+# 24. In Python, which index represents the first element of sequence?
+# the first element of sequence is 0 the second element is 1 and the third element is 2.
+
+# 25. How can you access the third element of a list in Python?
+# my_list[2] because 2 is the third element in list.
+
+# 26. What does slicing allow you to do with a sequence in Python?
+# slice() method extracts a section of data and returns it as new data, without modifying it.
+
+# 27. How can you extract a subsequence of a list from index 2 to index 5 (5 must be included) in Python?
+# list[2:6]
+
+# 28. What does the "step" value in slicing indicate?
+# The step parameter is used to specify the steps to take from start to end index.
+
+# 29. What is a reusable block of code in Python that performs a specific task called?
+# Python uses the name “function” to describe a reusable chunk of code.
+
+# 30. In Python, what are the values passed to a function called?
+# An argument is the value that are sent to the function when it is called.
+
+# 31. Which string method is used to convert a string to uppercase in Python?
+# upper() method is used to convert lowercase letters in a string to uppercase.
+
+# 32. What list method is used to add an element to the end of a list in Python?
+# You can add elements to a list using the append method. The append() method adds a single element towards the end of a list.
+
+# 33. In Python, which keyword is used to define a new function?
+# The def keyword is used to create, (or define) a function.
+
+# 34. What keyword is used to return a value from a function in Python?
+# return statement is a special statement that you can use inside a function or method to send the function's result back to the caller. A return statement consists of the return keyword followed by an optional return value.
