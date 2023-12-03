d9c04f52046ef3bc93231a01d00f344c870548b7
SkullSichinava <sichinava614@gmail.com>
2023-12-03 16:51:38 +0400

    first commit

---
+++ day 20/homework/lesson20.py
@@ -0,0 +1,32 @@
+def make_upper_case(s):
+    return s.upper()
+
+
+def areYouPlayingBanjo(name):
+    if name[0].lower() == 'r':
+        return name + ' plays banjo'
+    else:
+        return name + ' does not play banjo'
+    
+def find_needle(haystack):
+    needle_index = haystack.index("needle")
+    return "found the needle at position " + str(needle_index)
+result = ("haystack")  
+
+def sum_array(a):
+    sum = 0
+    for i in a:
+        sum += i
+    return sum
+
+def invert(lst):
+    result = list()
+    for num in lst:
+        result.append(-num)
+    return result
+
+def find_average(array):
+    if len(array) != 0:
+        return sum(array) / len(array)
+    else:
+        return 0
