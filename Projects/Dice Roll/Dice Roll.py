821b2f9671270abf5f190997c83fbca9c190129d
SkullSichinava <sichinava614@gmail.com>
2024-01-11 22:04:53 +0400

    first commit

--- Projects/Dice Roll/Dice Roll.py
+++ Projects/Dice Roll/Dice Roll.py
@@ -1,11 +1,18 @@
 import random
-dice=[1,2,3,4,5,6]
-roll_again = "yes"
-while roll_again == "yes" or roll_again == "y":
-    print("Rolling the dices...")
-    print("The values are....")
-    value1=random.choice(dice)
-    value2=random.choice(dice)
-    print(value1,value2)
-    roll_again = input("Press 'y' or 'yes' to roll the dices again.")
-print("Have a good day.")
+print("Hello, this is dice roller")
+choice=(input("Roll a dice? "))
+
+
+values=[1,2,3,4,5,6]
+if choice=="yes":
+    player2=random.choice(values)
+    player1=random.choice(values)
+    print("Player 1 rolled:", player1, "and Player2 rolled:", player2)
+    if player1==player2:
+        print("Draw!")
+    elif player1>player2:
+        print("Player 1 won!")
+    else:
+        print("Player 2 won!")
+else:
+    print("Good Luck!")
