7391b90991a0cb5f569b1815aec8449e2152e95a
SkullSichinava <sichinava614@gmail.com>
2024-04-26 02:19:20 +0400

    commit

--- day 56/classwork/lesson56.py
+++ day 56/classwork/lesson56.py
@@ -1,3 +1,5 @@
+# 8 kyu
+
 # HW1
 def litres(time):
     return time // 2
@@ -29,3 +31,59 @@ def fake_bin(x):
 # HW5
 def count_by(x, n):
     return [x * i for i in range(1, n + 1)]
+
+# 7 kyu
+
+# HW1
+def to_jaden_case(string):
+    words = string.split()
+    jaden_cased_words = [word.capitalize() for word in words]
+    return ' '.join(jaden_cased_words)
+
+# HW2
+def accum(st):
+    index = 1 
+    result = [] 
+    for l in st: 
+        l = l * index 
+        l = l.capitalize()
+        index += 1
+        result.append(l)
+    return "-".join(result)
+
+# HW3
+def remove_smallest(numbers):
+    x = list(numbers)
+    if x:
+        x.remove(min(numbers))
+    return x
+
+# 6 kyu
+
+# HW1
+def solution(number):
+    if number < 0:
+        return 0
+    return sum(x for x in range(number) if x % 3 == 0 or x % 5 == 0)
+
+# HW2
+def likes(names):
+    if len(names) == 0:
+        return "no one likes this"
+    elif len(names) == 1:
+        return "%s likes this" % names[0]
+    elif len(names) == 2:
+        return "%s and %s like this" % (names[0], names[1])
+    elif len(names) == 3:
+        return "%s, %s and %s like this" % (names[0], names[1], names[2])
+    else:
+        return "%s, %s and %s others like this" % (names[0], names[1], len(names)-2)
+
+# HW3
+def is_pangram(s):
+    sentence = s.lower()
+    letters_found = set()
+    for char in sentence:
+        if char.isalpha():
+            letters_found.add(char)
+    return len(letters_found) == 26
