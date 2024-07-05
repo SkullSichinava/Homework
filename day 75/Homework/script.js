67b630ff927f5f7c108cec5883c774edd8bd3109
SkullSichinava <sichinava614@gmail.com>
2024-07-05 14:52:32 +0400

    commit

--- day 75/Homework/script.js
+++ day 75/Homework/script.js
@@ -1,25 +1,25 @@
-// // Date
+// Date
 
-// // 1 davaleba
+// 1 davaleba
 // const d = new Date()
 // console.log(d)
-// // 2 davaleba
+// 2 davaleba
 // console.log(d.toTimeString())
-// // 3 davaleba
+// 3 davaleba
 // running = true;
-// while(running){
+//  while(running){
 //     day = parseInt(prompt("enter between 1 and 7"))
-//     if(day > 7 || day <= 0){
-//         window.alert("Please Enter Between 1 and 7")
-//     }
-//     else if(isNaN(day)){
-//         window.alert("Please enter a valid number");
-//     }
-//     else{
-//         if(day == 1){
-//             console.log("Monday")
-//         }
-//         else if(day == 2){
+//  if(day > 7 || day <= 0){
+//        window.alert("Please Enter Between 1 and 7")
+//    }
+//    else if(isNaN(day)){
+//       window.alert("Please enter a valid number");
+//           }
+//    else{
+//        if(day == 1){
+//            console.log("Monday")
+//        }
+//        else if(day == 2){
 //             console.log("Tuesday")
 //         }
 //         else if(day == 3){
@@ -40,14 +40,14 @@
 //         running = false
 //     }
 // }
-// // 4 davaleba
+// 4 davaleba
 // let date1 = new Date("04/09/2023")
 // let date2 = new Date("01/04/2022")
 // console.log(date1.getTime() - date2.getTime())
-// // 5 davaleba
+// 5 davaleba
 // // const d = new Date()
 // console.log(d.toLocaleDateString())
-// // 6 davaleba
+// 6 davaleba
 // const firstDate =  new Date("2021-04-09")
 // const secondDate = new Date("2021-05-19")
 
@@ -60,13 +60,13 @@
 // else{
 //     console.log(`${firstDate} and ${secondDate} are the same`)
 // }
-// // 7 davaleba
+// 7 davaleba
 // function days(month, year){
 //     return new Date(year, month,0).getDate()
 // }
 
 // console.log(days(4, 2010))
-// // 8 davaleba
+// 8 davaleba
 // function timeAgo(date){
 //     const now = new Date()
 //     const pastDate = new Date(date);
@@ -94,7 +94,7 @@
 // console.log(timeAgo("2024-05-03"))
 
 
-// // 9  davaleba
+// 9  davaleba
 // const birthday = new Date(2015, 3, 5)
 
 // const today = new Date()
@@ -118,8 +118,8 @@
 
 // console.log(getAge('2010/04/29'))
 
-// // Math
-// // 1 davaleba
+// Math
+// 1 davaleba
 // function getRandomInt(min, max){
 //     min = Math.ceil(min)
 //     max = Math.floor(max)
@@ -127,59 +127,59 @@
 // }
 
 // console.log(getRandomInt(1, 8))
-// // 2 davaleba
+// 2 davaleba
 // function squareRoot(number){
 //     return Math.sqrt(number)
 // }
 
 // console.log(squareRoot(9))
-// // 3 davaleba
+// 3 davaleba
 // function roundNumber(number){
 //     return Math.round(number)
 // }
 
 // console.log(roundNumber(5.5))
-// // 4 davaleba
+// 4 davaleba
 // function roundDown(number){
 //     return Math.floor(number)
 // }
 
 // console.log(roundDown(5.3))
 
-// // 5 davaleba
+// 5 davaleba
 // function roundUp(number){
 //     return Math.ceil(number)
 // }
 
 // console.log(roundUp(2.3))
 
-// // 6 davaleba
+// 6 davaleba
 // function c(A, B){
 //     return Math.pow(A, B)
 // }
 
 // console.log(c(2, 3))
-// // 7 davaleba
+// 7 davaleba
 // function absolute(number){
 //     return Math.abs(number)
 // }
 
 // console.log(absolute(-5))
-// // 8 davaleba
+// 8 davaleba
 // function getMaxNumber(array){
 //     return Math.max(null, array)
 // }
 
 // console.log(getMaxNumber([1, 2, 3, 4, 5]))
 
-// // 9 davaleba
+// 9 davaleba
 // function randomBoolean(){
 //     return Math.random() >= 0.5
 // }
 
 // console.log(randomBoolean())
 
-// // 10 davaleba
+// 10 davaleba
 // function randomRGBColor(){
 //     const r = Math.floor(Math.random() * 256)
 //     const g = Math.floor(Math.random() * 256)
@@ -189,8 +189,8 @@
 
 // console.log(randomRGBColor())
 
-// // setInterval
-// // 1 davaleba
+// setInterval
+// 1 davaleba
 // let seconds = 10
 // const interval = setInterval(function(){
 //     console.log(seconds + "second remaining")
@@ -200,7 +200,7 @@
 //         console.log("Time's over")
 //     }
 // }, 1000)
-// // 2 davaleba
+// 2 davaleba
 // const now = new Date()
 // const hours = now.getHours()
 // const minutes = now.getMinutes()
@@ -219,13 +219,13 @@
 //     console.log(hours + "," + minutes + "," + seconds)
 // }, 1000)
 
-// // 3 davaleba
+// 3 davaleba
 // const interval = setInterval(function(){
 //     const randomNumber = Math.floor(Math.random() * 100)
 //     console.log(randomNumber)
 // }, 2000)
 
-// // 4 davaleba
+// 4 davaleba
 // const interval = setInterval(function(){
 //     let date = new Date()
 //     let hrs = date.getHours()
@@ -234,8 +234,7 @@
 //     let time = `${hrs}:${mins}:${secs}`
 //     console.log(time)
 // }, 1000)
-
-// // 5 davaleba
+// 5 davaleba
 // const words = ["a", "b", "c", "d", "e", "f", "g"];
 
 // const interval = setInterval(function(){
@@ -260,6 +259,6 @@
 // }, 1000)
 // 3 davaleba
 
-setTimeout(function(){
-    console.log("Successs message hidden");
-}, 5000);
+// setTimeout(function(){
+//    console.log("Successs message hidden");
+// }, 5000);
